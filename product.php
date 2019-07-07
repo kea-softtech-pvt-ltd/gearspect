cfbab5a8fd083e2e0f8535937ca2fdcd02bdbbcb
yogeshkoradekea <yogeshkorade@keasofttech.com>
2019-07-07 14:40:48 +0530

    Yogesh : product page UI changes

--- product.php
+++ product.php
@@ -19,59 +19,288 @@
         </div>
 	</div>
 </section>
-<!--End breadcrumb area-->                                
+<!--End breadcrumb area-->     
 
+<style>                           
+.how-can-help-box .inner-content ul li {
+    position: relative;
+    display: block;
+    overflow: hidden;
+    border-bottom: 1px solid #e8e6f4;
+    padding: 5px 0px;
+    margin-bottom: 0px;
+}
+.how-can-help-box .inner-content ul {
+	margin-bottom: 0px !important;
+}
+
+.accordion {
+  background-color: #005fb3;
+  color: #fff;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+}
+
+.active, .accordion:hover {
+  background-color: #006dcc;
+}
+
+.accordion:after {
+  content: '\002B';
+  color: #fff;
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
+
+#accordion .active:after {
+  content: "\2212";
+}
+
+.collapse-info {
+	display:none;
+	border: 1px solid #ddd;
+}
+.card-body .active_c {
+	color:red !important;
+	background-color: #fff;
+}
+</style>
 <!--Start Project single area-->
-<section class="project-single-area">
+<section class="project-single-area" style="padding: 50px 0 56px">
     <div class="container">	
+		<div class="related-product" id="related-product">
+			<div class="title">
+				<h3>Gear Grinding Machines</h3>
+			</div>
+        </div>		
         <div class="row">
-            <div class="col-xl-6 col-lg-12">
-                <div class="project-carousel-box">
-                    <div class="project-single-carousel owl-carousel owl-theme">
-                        <div class="single-item">
-                            <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
-                        </div>
-                        <div class="single-item">
-                            <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
-                        </div>    
+			<div class="col-xl-3 col-lg-12 col-md-12 col-sm-12">
+                <div class="how-can-help-box">
+                    <div class="inner-content">
+						<div class="container">
+							<div id="accordion" class="">
+								<div class="card mb-0">
+									<div class="card-header collapsed accordion" id="accordionOne" data-toggle="collapse" href="#collapseOne">
+										<span>Gear Grinding Machines</span>
+									</div>
+									<div id="collapseOne" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li class="active_c" onclick="openTab(1);">SBO 340/400 CNC</li>
+											<li onclick="openTab(2);">SBP 400 CNC</li>
+											<li onclick="openTab(3);">SBP 800/1250/2000 CNC</li>
+										</ul>
+									</div>
+									<div class="card-header collapsed accordion" data-toggle="collapse" href="#collapseOne1">
+										<span>Gear Hobbing Machines</span>
+									</div>
+									<div id="collapseOne1" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li>SF 90 CNC</li>
+											<li>SBO SF SF 150/250/350/500 CNC</li>
+											<li>SFH 160/250 CNC</li>
+											<li>SF 800/1000 CNC</li>
+										</ul>
+									</div>
+									<div class="card-header collapsed accordion" data-toggle="collapse" href="#collapseOne2">
+										<span>Gear Shaping Machines</span>
+									</div>
+									<div id="collapseOne2" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li>OHA 16/32 CNC</li>
+											<li>SO 800 CNC</li>
+										</ul>
+									</div>
+									<div class="card-header collapsed accordion" data-toggle="collapse" href="#collapseOne3">
+										<span>Bevel Gear Grinding Machines</span>
+									</div>
+									<div id="collapseOne3" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li>SBK 350/600/1000 CNC</li>
+										</ul>
+									</div>
+									<div class="card-header collapsed accordion" data-toggle="collapse" href="#collapseOne4">
+										<span>Bevel Gear Hobbing Machines</span>
+									</div>
+									<div id="collapseOne4" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li>SFK 200 CNC</li>
+											<li>SFK 350/600/1000 CNC</li>
+										</ul>
+									</div>
+									<div class="card-header collapsed accordion" data-toggle="collapse" href="#collapseOne5">
+										<span>Gear Deburring Machines</span>
+									</div>
+									<div id="collapseOne5" class="card-body collapse" data-parent="#accordion" >
+										<ul>
+											<li>GT 600</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
                     </div>
-                </div>
+                </div>      
             </div>
-            <div class="col-xl-6 col-lg-12">
-                <div class="project-info" id="project-info">
-                    <div class="title">
-                        <h3>SBO 340/400 CNC</h3>
-                        <p>Highly productive gear generating grinder, newly designed with 7 or 8 control axes, dynamic balancing of grinding wheel and modern cooling system</p>
-                    </div>
-					<table class="table table-striped borderless">
-						<thead>
-							<th></th>
-							<th>Basic</th>
-							<th>High speed</th>
-						</thead>
-						<tbody>
-							<tr>
-								<td>Max. workpiece dia.</td>
-								<td>340/400 mm</td>
-								<td>340/400 mm</td>
-							</tr>
-							<tr>
-								<td>Max. module</td>
-								<td>6 mm</td>
-								<td>8 mm</td>
-							</tr>
-							<tr>
-								<td>Cutting speed</td>
-								<td>35 m/s</td>
-								<td>63 m/s</td>
-							</tr><tr>
-								<td>No. of CNC axes</td>
-								<td>7</td>
-								<td>8</td>
-							</tr>
-						<tbody>
-					</table>
-                </div>
+			<div class="col-xl-9 col-lg-12">
+				<div class="row collapse-info" id="collapseOne_1">
+					<div class="col-xl-7 col-lg-12 n-p-l f-l">
+						<div class="project-carousel-box">
+							<div class="project-single-carousel owl-carousel owl-theme">
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>    
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-5 col-lg-12 f-r">
+						<div class="project-info" id="project-info">
+							<div class="title">
+								<h3>SBO 340/400 CNC</h3>
+								<p>Highly productive gear generating grinder, newly designed with 7 or 8 control axes, dynamic balancing of grinding wheel and modern cooling system</p>
+							</div>							
+						</div>
+					</div>  
+					<div class="col-xl-12 col-lg-12">
+						<table class="table table-striped borderless">
+							<thead>
+								<th></th>
+								<th>Basic</th>
+								<th>High speed</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Max. workpiece dia.</td>
+									<td>340/400 mm</td>
+									<td>340/400 mm</td>
+								</tr>
+								<tr>
+									<td>Max. module</td>
+									<td>6 mm</td>
+									<td>8 mm</td>
+								</tr>
+								<tr>
+									<td>Cutting speed</td>
+									<td>35 m/s</td>
+									<td>63 m/s</td>
+								</tr><tr>
+									<td>No. of CNC axes</td>
+									<td>7</td>
+									<td>8</td>
+								</tr>
+							<tbody>
+						</table>	
+					</div>	
+				</div> 
+				<div class="row collapse-info" id="collapseOne_2">
+					<div class="col-xl-7 col-lg-12 n-p-l f-l">
+						<div class="project-carousel-box">
+							<div class="project-single-carousel owl-carousel owl-theme">
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>    
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-5 col-lg-12 f-r">
+						<div class="project-info" id="project-info">
+							<div class="title">
+								<h3>SBP 400 CNC</h3>
+								<p>Form Gear Grinding machine , 6 axes CNC controlled form horizontal gear grinder with high mechanical rigidity. High operating accuracy is ensured by temperature stabilization of machine's main parts.</p>
+							</div>							
+						</div>
+					</div>  
+					<div class="col-xl-12 col-lg-12">
+						<table class="table table-striped borderless">
+							<thead>
+								<th></th>
+								<th>SBP 400 CNC</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Max. workpiece dia.</td>
+									<td>400 mm</td>
+								</tr>
+								<tr>
+									<td>Max. module</td>
+									<td>12 mm</td>
+								</tr>
+								<tr>
+									<td>Max. face width</td>
+									<td>380 mm</td>
+								</tr>
+								<tr>
+									<td>Max. spindle travel</td>
+									<td>650 mm</td>
+								</tr>
+							<tbody>
+						</table>	
+					</div>	
+				</div> 
+				<div class="row collapse-info" id="collapseOne_3">
+					<div class="col-xl-7 col-lg-12 n-p-l f-l">
+						<div class="project-carousel-box">
+							<div class="project-single-carousel owl-carousel owl-theme">
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>
+								<div class="single-item">
+									<img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">    
+								</div>    
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-5 col-lg-12 f-r">
+						<div class="project-info" id="project-info">
+							<div class="title">
+								<h3>SBP 800/1250/2000 CNC</h3>
+								<p>Form Gear Grinding machine, 6 axes CNC controlled profile vertical gear grinder with high mechanical rigidity. High operating accuracy is ensured by temperature stabilization of machine's main parts.</p>
+							</div>							
+						</div>
+					</div>  
+					<div class="col-xl-12 col-lg-12">
+						<table class="table table-striped borderless">
+							<thead>
+								<th></th>
+								<th>SBP 800 CNC</th>
+								<th>SBP 1250 CNC</th>
+								<th>SBP 2000 CNC</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Max. workpiece dia</td>
+									<td>800 mm</td>
+									<td>1250 mm</td>
+									<td>2000 mm</td>
+								</tr>
+								<tr>
+									<td>Max. module</td>
+									<td>20 mm</td>
+									<td>25 mm</td>
+									<td>35 mm</td>
+								</tr>
+								<tr>
+									<td>Max. tooth width</td>
+									<td>400 mm</td>
+									<td>700 mm</td>
+									<td>1000 mm</td>
+								</tr>
+							<tbody>
+						</table>	
+					</div>	
+				</div> 				
             </div>   
         </div>
     </div>
@@ -84,96 +313,6 @@
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                 <div class="shop-content">
-                    <!--Start single shop content-->
-                    <div class="single-shop-content">
-                        <div class="row">                           
-                            <div class="col-lg-6">
-                                <div class="single-product-carousel">
-                                    <div class="carousel-outer">
-                                        <div class="content-carousel owl-carousel owl-theme">
-                                            <div class="content-slide">
-                                                <div class="content-inner">
-                                                    <div class="product-main-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="content-slide">
-                                                <div class="content-inner">
-                                                    <div class="product-main-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="content-slide">
-                                                <div class="content-inner">
-                                                    <div class="product-main-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="thumbs-outer">
-                                            <ul class="thumbs-carousel owl-carousel owl-theme">
-                                                <li>
-                                                    <div class="thumb-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="thumb-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>    
-                                                </li>
-                                                <li>
-                                                    <div class="thumb-image">
-                                                        <img src="images/projects/project-single/project-info-1.jpg" alt="Awesome Image">
-                                                    </div>    
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>    
-                            </div>                              
-                            <div class="col-lg-6">
-                                <div class="content-box">
-                                    <div class="title">
-										<h3>SBO 340/400 CNC</h3>
-										<p>Highly productive gear generating grinder, newly designed with 7 or 8 control axes, dynamic balancing of grinding wheel and modern cooling system</p>
-									</div>
-									<table class="table table-striped borderless">
-										<thead>
-											<th></th>
-											<th>Basic</th>
-											<th>High speed</th>
-										</thead>
-										<tbody>
-											<tr>
-												<td>Max. workpiece dia.</td>
-												<td>340/400 mm</td>
-												<td>340/400 mm</td>
-											</tr>
-											<tr>
-												<td>Max. module</td>
-												<td>6 mm</td>
-												<td>8 mm</td>
-											</tr>
-											<tr>
-												<td>Cutting speed</td>
-												<td>35 m/s</td>
-												<td>63 m/s</td>
-											</tr><tr>
-												<td>No. of CNC axes</td>
-												<td>7</td>
-												<td>8</td>
-											</tr>
-										<tbody>
-									</table>   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--End single shop content-->
                     <!--Start related product box-->
                     <div class="related-product" id="related-product">
                         <div class="title">
@@ -229,3 +368,37 @@
 <!--End shop area-->
 
 <?php include_once('footer.php'); ?>
+<script>
+var i;
+var acc = document.getElementsByClassName("accordion");
+for (i = 0; i < acc.length; i++) {		
+	acc[i].addEventListener("click", function() {
+		$('.accordion').removeClass("active");  
+		this.classList.toggle("active");
+	});
+}
+
+function openTab(_id) {
+	$('.collapse-info').hide();
+	$('#collapseOne_'+_id).show();
+}
+
+$(document).ready(function(){
+	
+	openTab(1);
+	
+	$('#accordionOne').click();
+	
+	// When we click on the LI
+	$("li").click(function(){
+	  // If this isn't already active
+	  if (!$(this).hasClass("active_c")) {
+		// Remove the class from anything that is active
+		$("li.active_c").removeClass("active_c");
+		// And make this active
+		$(this).addClass("active_c");
+	  }
+	});
+	
+})
+</script>
