a92d02e7511bc6a97b76c3127f057a26a53d20b7
Roshan Deorukhkar <roshandeorukhkar@gmail.com>
2019-07-08 09:40:15 +0530

    Roshan - front page image changes

---
+++ career.php
@@ -0,0 +1,25 @@
+<?php
+    $pageWords = "Gear cutting machine, gear measuring instruments, gearspect, Gear Hobbing Machines, Gear Shaping Machines, Bevel Gear Grinding Machines, geometric deviation of cylindrical gears and gear cutting tools";
+    $pageDescription = "GearSpect-We are leading gear service provider";
+    $pageTitle = "GearSpect - Career";
+?>
+<?php include_once('header.php'); ?>   
+
+<!--Start breadcrumb area-->     
+<section class="breadcrumb-area" style="background-image: url(images/resources/breadcrumb-bg.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="inner-content clearfix">
+                    <div class="title text-center">
+                       <h1>Career</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+</section>
+<!--End breadcrumb area-->     
+
+
+<?php include_once('footer.php'); ?>
